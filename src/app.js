6aa70800194806349cabb2edc05ad8ff2e154851
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-05 18:11:48 +0530

    Added nodemon in my project for my convenience

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 const express = require("express")
 const app = express()
-app.listen(3000)
+app.use("/test",(req, res) => {
+    res.send('Hello from the Server')
+
+})
+app.listen(3000, () => {
+    console.log("Server is successfully listening on port 3000")
+})
