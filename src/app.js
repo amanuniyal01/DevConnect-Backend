635acb681a6b373b6d0c3542d3ba9169282231d1
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-06 11:59:32 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require("express")
 const app = express()
-app.use("/test",(req, res) => {
+app.get("/test/:userId", (req, res) => {
+    console.log(req.params)
+    res.send({ firstName: "Aman", lastName: "Uniyal" })
+
+})
+app.get("/", (req, res) => {
     res.send('Hello from the Server')
 
+})
+app.get("/hello", (req, res) => {
+    res.send("Hello Hello Hello")
+})
+
+
+app.post("/hello", (req, res) => {
+    // Saving data to DB
+    res.send("Data successfully saved to Database")
 })
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000")
