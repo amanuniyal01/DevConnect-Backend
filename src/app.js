a4e20a87b2523f16ac8206f593bd1aee3f702be2
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-28 12:51:02 +0530

    Handled error

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require("express")
 const app = express()
 const ConnectDB = require("./config/database")
 const User = require("./models/user")
+// Middleware express.json
 app.use(express.json())
 
 app.post("/signup", async (req, res) => {
 
-    // Creating a new instance of the User model
+    // Creating a new instance of the User model  
     const user = new User(req.body)
 
     // Save the document to the database
@@ -15,6 +16,27 @@ app.post("/signup", async (req, res) => {
 
 })
 
+// GET ONE USER
+app.get("/user", async (req, res) => {
+    const email = req.body.email;
+    try {
+        const users = await User.find({ email })
+        if(users.length===0){
+            res.send("No user found")
+        }
+        res.send(user)
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+
+    }
+
+
+})
+// Feed API - GET/feed - get all the users from the database
+app.get("/feed", (req, res) => {
+    res.send("Data ")
+})
 
 
 ConnectDB().then(() => {
