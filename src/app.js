703d60e401b94537690bbb66e9a4a1c80538f912
amanuniyal01 <amanuniyal315@gmail.com>
2026-03-19 19:51:36 +0530

    Added validation libraries for password,gmail and photUrl.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@ const express = require("express")
 const app = express()
 const ConnectDB = require("./config/database")
 const User = require("./models/user")
+// const data=req.body
 // Middleware express.json
+const ALLOWED_UPDATES = [
+    "firstName",
+    "lastName",
+    "age",
+    "gender",
+    "photoUrl",
+    "skills",
+    "about"
+
+]
+
 app.use(express.json())
 
 app.post("/signup", async (req, res) => {
+    const { firstName, lastName, email, password, age } = req.body;
+    if (!firstName || !lastName || !email || !password || !age) {
+        return res.status(400).send("All required fields must be filled")
+    }
+    // Email basic validation
+    if (!email.includes("@")) {
+        return res.status(400).send("Invalid email format");
+    }
+    // Age Validation
+    if (age && age < 0) {
+        return res.status(400).send("Age cannot be negative");
+    }
+    //  Password validation
+    if (password.length < 6) {
+        return res.status(400).send("Password must be at least 6 characters");
+    }
 
     // Creating a new instance of the User model  
     const user = new User(req.body)
@@ -75,17 +103,33 @@ app.get("/feed", async (req, res) => {
 
     }
 })
-app.patch("/user", async (req, res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
     const data = req.body;
 
     try {
-        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "before" });
-        console.log(user)
+           if (Object.keys(data).length === 0) {
+            return res.status(400).send("No data provided");
+        }
+
+        if (data.skills && data.skills.length > 10) {
+            return res.status(400).send("Maximum 10 skills allowed");
+        }
+        if (data.age && data.age < 0) {
+            return res.status(400).send("Age cannot be Negative")
+        }
+       
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k))
+        if (!isUpdateAllowed) {
+            throw new Error("Update Not Allowed")
+        }
+
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "before", runValidator: true });
+
         res.send("User updated successfully")
 
     } catch (err) {
-        res.status(400).send("Something went wrong")
+        res.status(400).send("Update Failed!" + err.message)
     }
 }
 )
--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,96 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 // Define the User schema
-const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true, minLength: 4 },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-    age: { type: Number, min: 0 },
-    gender: { type: String, enum: ['Male', 'Female', 'Other'], },
-    password: { type: String, select: false, trim: true, minLength: 6 },
-    photoUrl: { type: String, default: "https://www.prakrutimaa.com/images/profile.jpg" },
-    about: { type: String, default: "Dev is in Search for someone here" },
-    skills: {
-        type: [String]
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, "First name is required"],
+            minlength: 3,
+            maxlength: 30,
+            trim: true
+        },
+
+        lastName: {
+            type: String,
+            required: [true, "Last name is required"],
+            minlength: 3,
+            maxlength: 30,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Please Use a Valid Email")
+                }
+            }
+        },
+
+        age: {
+            type: Number,
+            min: [0, "Age cannot be negative"],
+            max: [120, "Age seems invalid"]
+        },
+
+        gender: {
+            type: String,
+            enum: {
+                values: ["Male", "Female", "Other"],
+                message: "Gender must be Male, Female or Other"
+            },
+            default: "Other"
+        },
+
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minlength: 6,
+            select: false,
+            validate(value) {
+                if (!validator.isStrongPassword(value)) {
+                    throw new Error("Please use a Strong Password..")
+                }
+            }
+        },
+
+        photoUrl: {
+            type: String,
+            default: "https://www.prakrutimaa.com/images/profile.jpg",
+            validate(value) {
+                if (validator.isURL(value)) {
+                    throw new Error("Please use a valid URL.")
+                }
+            }
+
+        },
+
+        about: {
+            type: String,
+            maxlength: 200,
+            default: "Developer exploring opportunities"
+        },
+
+        skills: {
+            type: [String],
+            validate: {
+                validator: function (arr) {
+                    return arr.length <= 10;
+                },
+                message: "Maximum 10 skills allowed"
+            }
+        }
+    },
+    {
+        timestamps: true
     }
-});
+);
 
 // Create a User model from the schema(Always Starts with a capital letter)
 const User = mongoose.model('User', userSchema);
