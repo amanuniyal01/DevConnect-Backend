a9df3641c0cc6acef453f856d771027a038485b8
amanuniyal01 <amanuniyal315@gmail.com>
2026-03-20 18:00:45 +0530

    Validate apis using validator library in diff file

---
+++ src/utils/validation.js
@@ -0,0 +1,18 @@
+const validator = require('validator')
+const ValidateSignupData = (req) => {
+    const { firstName, lastName, password, email } = req.body;
+    if (!firstName || !lastName) {
+        throw new Error("Name is Required")
+    }
+    else if (firstName.length < 4 || firstName.length > 50 || lastName.length < 2 || lastName.length > 50) {
+        throw new Error("Invalid userName please use a valid Name.")
+    }
+    else if (!validator.isEmail(email)) {
+        throw new Error("Email is not valid.")
+    }
+    else if (!validator.isStrongPassword(password)) {
+        throw new Error("Please use a strong password")
+    }
+
+}
+module.exports=ValidateSignupData()
