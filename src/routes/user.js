258fc19354a759eeb91f2c8ba5f9b5bb83fd4e59
amanuniyal01 <amanuniyal315@gmail.com>
2026-04-26 20:25:11 +0530

    Initalize api for getting all user Connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,10 +10,25 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
             receiverUserId: loggedInUser._id,
             status: "interested"
         }).populate(
-            "senderUserId",["firstName","lastName"]
+            "senderUserId", ["firstName", "lastName"]
         )
         res.json({ message: "Data fetched Successfully", data: receivedConnectionRequests })
 
+    }
+    catch (err) {
+        res.status(400).send("error: " + err.message)
+    }
+
+
+})
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connections = await connectionRequest.find({
+            fromUserId: loggedInUser._id,
+
+            status: "accepted"
+        })
     }
     catch (err) {
         res.status(400).send("error: " + err.message)
